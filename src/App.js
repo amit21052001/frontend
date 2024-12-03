e8535ea4a8f5209a0d7a1aa4f31b0b0b5b42809c
amit21052001 <amit.solanki01@sap.com>
2024-12-03 19:51:20 +0530

    small fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import SignUp from "./components/SignUp";
 import AdminPage from "./components/AdminPage";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/admin"  element={<AdminPage />} />
+        <Route path="/home" element={<HomePage />} /> 
       </Routes>
     </Router>
   );
