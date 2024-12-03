ec5a314239b7a4a8d386a6e962543650dc939ad6
Mishra, Utkarsh <utkarsh.mishra01@sap.com>
2024-12-03 19:40:20 +0530

    adding admin page changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import SignUp from "./components/SignUp";
+import AdminPage from "./components/AdminPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/admin"  element={<AdminPage />} />
       </Routes>
     </Router>
   );
